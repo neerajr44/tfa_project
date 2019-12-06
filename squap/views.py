449db76778feaa2f4a61bec202ce0ae63a98ee1c
Neeraj <neerajr44@live.com>
2019-12-06 22:01:13 +0000

    added map view

--- squap/views.py
+++ squap/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Squirrel
 
-def index(request):
+"""idef index(request):
     return HttpResponse("View is working well.")
+"""
 
+def view_map(request):
+    sighting = Squirrel.objects.all()[:25]
+    context = {
+            'sightings': sighting,
+            }
+    return render( request, 'squap/map.html', context)
 # Create your views here.
