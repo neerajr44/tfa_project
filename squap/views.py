c941bb3303a22b2d90a8d2ef296f65feaebd8db2
Neeraj <neerajr44@live.com>
2019-12-07 06:01:17 +0000

    Adding map flags

--- squap/views.py
+++ squap/views.py
@@ -12,4 +12,22 @@ def view_map(request):
             'sightings': sighting,
             }
     return render( request, 'squap/map.html', context)
+
+def stats_page(request):
+    am_count = 0
+    pm_count = 0
+    running_count = 0
+    foraging_count = 0
+    approaches_count = 0
+    moans_count = 0
+    sq_count = len( Squirrel.objects.distinct('Unique_squirrel_ID'))
+    context = {'squirrel_count' : sq_count,
+              'AM_count' : am_count,
+              'PM_count' : pm_count,
+              'Running_count' : running_count,
+              'Foraging_count' : foraging_count,
+              'Approaches_count' : approaches_count,
+              'Moans_count' : moans_count,
+              }
+    return render(request, 'squap/stats.html', context)
 # Create your views here.
