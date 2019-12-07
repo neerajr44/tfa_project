ba1f0cb9cfa6ef280f3c0d4ed00e9220483bd031
Isha Gupta <ig2412@columbia.edu>
2019-12-07 06:48:42 +0000

    Added 3 html files

--- squap/urls.py
+++ squap/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name='squap'
+
 urlpatterns = [
         path('map/', views.view_map, name='map'),
+        path(
         path('sightings/stats', views.stats_page, name = 'stats')
         ]
